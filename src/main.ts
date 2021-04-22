0128ccb2d7d22cd450216d77676d735f67d44956
Pham Hoang <phamhoang.dev@gmail.com>
2021-04-22 17:16:30 +0700

    Init project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+/** @format */
 
-createApp(App).use(store).use(router).mount("#app");
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './index.css';
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+
+app.mount('#app');
